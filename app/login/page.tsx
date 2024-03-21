b5db575516632cab2ef6d63072e2138bf89c7fb1
System Administrator <ma.91.02@mail.ru>
2024-03-21 18:48:37 +0300

    edit style login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,15 +4,15 @@ import './login.css'
 import ButtonContainer from '@/components/btn-ont'
 export default function LogIn() {
   return (
-    <main className='flex flex-col xl:flex-row bg-gradient-to-tr from-[#222] via-[#990607]/50 px-5 xl:px-[50px] to-[#444]  justify-center items-center h-screen'>
-      <div className='flex flex-col h-[50%] xl:h-full items-center justify-center w-full'>
+    <main className='flex flex-col xl:flex-row bg-gradient-to-tr from-[#000] via-[#990607]/40 px-5 xl:px-[50px] to-[#444]  justify-center items-center h-screen'>
+      <div className='flex flex-col h-[50%] xl:h-full items-center justify-end xl:justify-center w-full'>
         <div className=' max-w-[250px] xl:max-w-[400px] '>
         <Image className=' ' src={'/images/logo.png'} width={400} height={400} alt='' />
         </div>
         <h1 className='almarai-bold text-3xl xl:text-4xl mb-2 md:mb-10 text-center'>مرو ام الاختراع ترحب بكم</h1>
         <h2 className=' text-2xl xl:text-3xl mb-10 almarai-bold'>صفحة تسجيل الدخول</h2>
       </div>
-      <div className="h-[50%]  py-10 flex justify-center items-center w-full">
+      <div className="h-[50%]  xl:py-10 flex justify-center items-start xl:items-center w-full">
         <form className='flex flex-col py-10 justify-center items-center'>
           <ButtonContainer>
             <span></span>
